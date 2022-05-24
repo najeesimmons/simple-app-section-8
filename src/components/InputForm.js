000dfd390643a03ff63323adbdc4893263fc70d8
najeesimmons <najee.bryant.simmons@gmail.com>
2022-05-24 13:56:34 -0400

    built basic archirecture

---
+++ src/components/InputForm.js
@@ -0,0 +1,17 @@
+import AddButton from "./UI/AddButton";
+
+const InputForm = () => {
+  return (
+    <div>
+      <form>
+        <label>Username</label>
+        <input></input>
+        <label>Age (Years)</label>
+        <input></input>
+        <AddButton></AddButton>
+      </form>
+    </div>
+  );
+};
+
+export default InputForm;
---
+++ src/components/UI/AddButton.js
@@ -0,0 +1,6 @@
+const AddButton = () => {
+    return (
+        <button type="submit">Add User</button>
+    )
+}
+export default AddButton
