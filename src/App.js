a5f97690e37f5cbc11c91d5ac281b88469c7aa9a
najeesimmons <najee.bryant.simmons@gmail.com>
2022-05-24 15:07:41 -0400

    laying this one down

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import InputForm from "./components/InputForm";
 import styles from "./App.module.css";
+import { useState } from 'react'
 
 function App() {
+  const [allUsers, setAllUsers] = useState('')
+
+  const handleAddUser = (enteredUser) => {
+    setAllUsers(prevUsers => {
+      const updatedUsers = [...prevUsers];
+      updatedUsers.unshift({id: Math.random().toString()})
+      return updatedUsers
+    })
+  }
+
   return (
     <div className={styles.app}>
-        <InputForm />
+        <InputForm onAddUser={handleAddUser}/>
     </div>
   );
 }
